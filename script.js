4060f83208af213e30f4993fda40e6cd39e0d96f
Ignacio <185011694+nimbusphagia@users.noreply.github.com>
2025-02-08 10:49:20 -0500

    Integrated checkWinners to gameplay function

--- script.js
+++ script.js
@@ -12,85 +12,73 @@ function Gameboard(){
         }
 
     const getBoard = () => board;
-    const makeMove = (player, row, square) => board[--row][--square] = player.token; 
-
+    const makeMove = (player, row, square) => board[row][square] = player.token;//NEED LOGIC TO NOT REWRITE SQUARES 
     return {getBoard, makeMove};
 }
 
 function GameController(playerOne = "Player One", playerTwo = "Player Two"){
     const board = Gameboard();
-    const players = [{name: "Player1", token: 1}, {name: "Player 2", token: 2}];
+    const players = [{name: "Player 1", token: 1}, {name: "Player 2", token: 2}];
     let activePlayer = players[0];
-
+    const tripleVerification = [false,false,false];
     const switchPlayerTurn = () => activePlayer = activePlayer === players[0] ? players[1] : players[0];
     const getActivePlayer = () => activePlayer;
-    const checkWinner = () => {
-        
-    };
 
+    function winOrLose(activePlayer){
+        if (tripleVerification[0] && tripleVerification[1] && tripleVerification[2]){
+            console.log(activePlayer.name + " won!", board.getBoard());
+            return true;
+        } else{
+            tripleVerification[0] = false;
+            tripleVerification[1] = false;
+            tripleVerification[2] = false;
+            return false;
+        } 
+    }
+    function winningPositions (func, arr, player, num){
+        if(num == 3){
+            for (let i = 0; i < 3; i++){
+                func(arr, player, i, i);
+            }
+        } else if(num == 4){
+            for (let i = 0; i < 3; i++){
+                func(arr.toReversed(), player, i, i);
+            }
+            } else{
+            for (let i = 0; i < 3; i++){
+                func(arr, player, i, num);
+            }
+        }
+    } 
+    function individualVerification(arr, activePlayer, loopIteration, arrIteration){
+        arr[loopIteration][arrIteration] == activePlayer.token ? tripleVerification[loopIteration] = true: tripleVerification[loopIteration] = false;
+    }
+    function checkWinners(activePlayer){
+        for(let i = 0; i < 5; i++){
+            winningPositions(individualVerification, board.getBoard(), activePlayer, i);
+            if(winOrLose(activePlayer)){
+                //here we can stop the round
+                break;
+            };
+        }
+    }
     const playRound = (row, square) => {
         board.makeMove(getActivePlayer(), row, square);
+        checkWinners(getActivePlayer());
         switchPlayerTurn();
-
     }
 
     return {getActivePlayer, playRound};
 }
 
-const test = [
-            [1,0,1],
-            [0,0,1],
-            [0,0,1]];
-let token = 1;
+const playGame = GameController();
+playGame.playRound(0,0);
+playGame.playRound(1,1);
+playGame.playRound(1,0);
+playGame.playRound(2,1);
+playGame.playRound(2,0);
+console.log();
 
-function calcConditions(){ //"TEST" = BOARD; "TOKEN" = PLAYERS.TOKEN; RETURN CHECKWINNER
-    const tripleCheck = [false,false,false];
-
-    
-    function individualVerification(arr, token, loopIteration, arrIteration){
-        arr[loopIteration][arrIteration] == token ? tripleCheck[loopIteration] = true: tripleCheck[loopIteration] = false;
-    }
-    function loopItems (func, test, token, num){
-        if(num == 3){
-            for (let i = 0; i < 3; i++){
-                func(test, token, i, i);
-            }
-        } else if(num == 4){
-            for (let i = 0; i < 3; i++){
-                func(test.toReversed(), token, i, i);
-            }
-            } else{
-            for (let i = 0; i < 3; i++){
-                func(test, token, i, num);
-            }
-        }
-        
-    }
-    function winOrLose(){//USE FILTER
-        if (tripleCheck[0] && tripleCheck[1] && tripleCheck[2]){
-            console.log("you won!", test);
-            return true;
-        } else{
-            tripleCheck[0] = false;
-            tripleCheck[1] = false;
-            tripleCheck[2] = false;
-            return false;
-        } 
-    }
-    function checkWinners(){
-        for(let i = 0; i < 5; i++){
-            loopItems(individualVerification, test, token, i);
-            if(winOrLose()){
-                break;
-            };
-
-        }
-    }
-        checkWinners();
-
-    return tripleCheck;//outcome;
-}
-console.log(calcConditions())
 
 
 
